69694999a283c3400f3e96581ff79a514c842374
root <root@domU-12-31-38-00-5A-02.compute-1.internal>
2011-10-04 16:59:41 +0000

    Added identity-cookbook and helloworld recipe

---
+++ cookbooks/identity-cookbook/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "8600100@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures identity-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+recipe "identity-cookbook::helloworld", "My first recipe, prints Hello World to the RightScale dashboard"
